fc4f14a2b79aa02da3a785157388df05571fa828
Anupam Gupta <anupam.gupta@aol.com>
2017-09-10 20:58:00 -0500

    Reverse a linked list Iterative
    Reverse a linked list Iterative

---
+++ 2.Linked Lists/07 Reverse a linked list Iterative.java
@@ -0,0 +1,25 @@
+/*
+  Reverse a linked list and return pointer to the head
+  The input list will have at least one element  
+  Node is defined as  
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    // This is a "method-only" submission. 
+    // You only need to complete this method. 
+Node Reverse(Node head) {
+    Node currNode = head;
+    Node prevNode = null;
+    Node nextNode = head.next;
+    
+    while(currNode != null){
+        nextNode = currNode.next;
+        currNode.next = prevNode;
+        prevNode = currNode;
+        currNode = nextNode;        
+    }
+    
+    return prevNode;
+}
